ccadd6a8ee3265e8b9a5aeba6a99a842af0f542e
ZenT3600 <40890172+ZenT3600@users.noreply.github.com>
2020-01-04 12:22:33 +0100

    Added Beatmap Functionality

--- api.py
+++ api.py
@@ -16,6 +16,77 @@ class Global:
         "S",
         "A"
     ]
+    diffstats = [
+        "length",
+        "bpm",
+        "circle num",
+        "slider num",
+        "circle size",
+        "hp drain",
+        "accuracy",
+        "ar",
+        "sr"
+    ]
+    mapstats = [
+        "title",
+        "author",
+        "mapper",
+        "status"
+    ]
+    likes = [
+        "plays",
+        "likes"
+    ]
+
+
+# --------------
+# MAPS Down Here
+# --------------
+
+
+# Base Mapset URL --> https://osu.ppy.sh/beatmapsets/{CODE}
+def return_mapset_page(code):
+    session = HTMLSession()
+    req = session.get(f"https://osu.ppy.sh/beatmapsets/{code}")
+    req.html.render()
+    return req
+
+
+def return_specific_mapset_difficulty(code, diff_code):
+    session = HTMLSession()
+    req = session.get(f"https://osu.ppy.sh/beatmapsets/{code}{diff_code}")
+    req.html.render()
+    return req
+
+
+# <a class="beatmapset-beatmap-picker__beatmap">
+def get_mapset_difficulty_codes(req):
+    return [code.attrs["href"] for code in req.html.find(".beatmapset-beatmap-picker__beatmap")]
+
+
+def get_difficulty_stats(req):
+    return dict(zip(Global.diffstats, [
+        stat.text for stat in req.html.find(".beatmap-basic-stats__entry")] + [stat.text for stat in req.html.find(".beatmap-stats-table__value")
+                    ]))
+
+
+def get_mapset_description(req):
+    return req.html.find(".beatmapset-info__description")[0].text
+
+
+def get_mapset_stats(req):
+    return dict(zip(Global.mapstats, [
+        stat.text for stat in req.html.find(".beatmapset-header__details-text")] +
+                    [req.html.find(".beatmapset-mapping__user")[0].text] +
+                    [req.html.find(".beatmapset-status")[0].text]))
+
+
+def get_mapset_likes(req):
+    return dict(zip(Global.likes, [like.text for like in req.html.find(".beatmapset-header__value-name")]))
+
+# -----------------
+# PLAYERS Down Here
+# -----------------
 
 
 # Base User URL --> https://osu.ppy.sh/users/{USERMAME}
@@ -107,14 +178,28 @@ def get_player_favorite_maps(req):
 
 
 if __name__ == '__main__':
-    req = return_player_profile(input("User: "))
+    # req = return_player_profile(input("User: "))
+    # print()
+    # print(f"Stats: {get_player_stats(req)}\n")
+    # print(f"Score Count: {get_player_score_count(req)}\n")
+    # print(f"Best Score: {get_player_best_score(req)}\n")
+    # print(f"First Places: {get_player_first_place_plays(req)}\n")
+    # print(f"Most Played Map: {get_player_most_played_map(req)}\n")
+    # print(f"Favorite Maps: {get_player_favorite_maps(req)}\n")
+    # print(f"PlayStyle: {get_player_playstyle(req)}\n")
+    # print(f"Socials: {get_player_socials(req)}\n")
+    # print(f"About: {get_player_about(req)}\n")
+    # print("\n\n\n")
+    
+    # Test Code: 1078502
+    code = input("Code: ")
+    map = return_mapset_page(code)
     print()
-    print(f"Stats: {get_player_stats(req)}\n")
-    print(f"Score Count: {get_player_score_count(req)}\n")
-    print(f"Best Score: {get_player_best_score(req)}\n")
-    print(f"First Places: {get_player_first_place_plays(req)}\n")
-    print(f"Most Played Map: {get_player_most_played_map(req)}\n")
-    print(f"Favorite Maps: {get_player_favorite_maps(req)}\n")
-    print(f"PlayStyle: {get_player_playstyle(req)}\n")
-    print(f"Socials: {get_player_socials(req)}\n")
-    print(f"About: {get_player_about(req)}\n")
+    diffs = get_mapset_difficulty_codes(map)
+    print(f"Difficulties: {diffs}")
+    diff = return_specific_mapset_difficulty(code, diffs[2])
+    print(f"Difficulty selected: {diff.url}")
+    print(f"Diff Stats: {get_difficulty_stats(diff)}")
+    print(f"Map Stats: {get_mapset_stats(map)}")
+    print(f"Map Likes: {get_mapset_likes(map)}")
+    print(f"Description: {get_mapset_description(map)}")
